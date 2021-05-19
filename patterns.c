db67487e8aa7d96e6c46c76908e91e644e582a99
annm-github <71374618+annm-github@users.noreply.github.com>
2021-05-19 10:41:59 +0530

    Create patterns.c
    patterns

---
+++ patterns.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <cs50.h>
+
+void drawline2(int nhash,int max)
+    {
+    int i;
+    int nspace=max-nhash;
+    for(i=0;i<nspace;i++)
+    {
+        printf(" ");
+    }
+    for(i=0;i<nhash;i++){
+        printf("#");
+    }
+    printf("\n");
+    }
+
+void drawline1(int m)
+    {
+    int i;
+    for(i=0;i<m;i++)
+    {
+        printf("#");
+    }
+    printf("\n");
+    }
+
+int main(void)
+{
+    int v=get_int("Enter type of pattern(1 or 2) ");
+    int n=get_int("Number of rows ");
+    int i;
+    if(v==1)
+    {
+    for(i=n;i>0;i--)
+    {
+        drawline1(i);
+    }
+    }
+    else if(v==2)
+    {
+        for(i=1;i<n+1;i++)
+        {
+            drawline2(i,n);
+        }
+    }
+    
+}
