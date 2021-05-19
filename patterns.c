6d16953b70787da46402613814d1d1d4efd145f2
annm-github <71374618+annm-github@users.noreply.github.com>
2021-05-19 11:04:09 +0530

    Update patterns.c

--- patterns.c
+++ patterns.c
@@ -26,7 +26,7 @@ void drawline1(int m)
 
 int main(void)
 {
-    int v=get_int("Enter type of pattern(1 or 2)");
+    int v=get_int("Enter type of pattern(1 or 2) ");
     int n=get_int("Number of rows");
     int i;
     if(v==1)
